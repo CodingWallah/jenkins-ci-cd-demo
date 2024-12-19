bfadfd452a19566fa1209bad7df10655bdb033a0
Coding Wallah <121433611+CodingWallah@users.noreply.github.com>
2024-12-19 12:25:44 +0530

    Update 5 Test.java

--- Test.java
+++ Test.java
@@ -1,6 +1,6 @@
 public class Test{
 
   public static void main(String[] args){
-    System.out.println("Hello, I am Java program : v4 ");
+    System.out.println("Hello, I am Java program : v5 ");
   }
 }
